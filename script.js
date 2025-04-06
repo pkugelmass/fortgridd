670619704ea1e269ae6f361c4ee15c7c9a63d441
Paul Kugelmass <pkugelmass@gmail.com>
2025-04-06 12:24:44 -0400

    Initial boilerplate content.

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Game script loaded!");
+
+// Example: Get the canvas element
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+// You can start drawing or setting up game logic here
+ctx.fillStyle = 'red';
+ctx.fillRect(10, 10, 50, 50); // Draw a small red square
